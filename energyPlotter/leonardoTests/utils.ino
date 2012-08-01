78a2d953df8b5b36fb5a23a2be1061cd1775ea8b
matthew venn <matt@mattvenn.net>
2012-08-01 09:59:20 +0100

    utils for tests

---
+++ energyPlotter/leonardoTests/utils.ino
@@ -0,0 +1,44 @@
+int serReadInt()
+{
+  int i, serAva;                           // i is a counter, serAva hold number of serial available
+  char inputBytes [7];                 // Array hold input bytes
+  char * inputBytesPtr = &inputBytes[0];  // Pointer to the first element of the array
+
+  if (Serial.available()>0)            // Check to see if there are any serial input
+  {
+    delay(5);                              // Delay for terminal to finish transmitted
+    // 5mS work great for 9600 baud (increase this number for slower baud)
+    serAva = Serial.available();  // Read number of input bytes
+    for (i=0; i<serAva; i++)   
+    {
+      char readChar = (char)Serial.read();   // Load input bytes into array
+      if( readChar == ',' )
+        break;
+      inputBytes[i] = readChar;
+    }
+    inputBytes[i] =  '\0';             // Put NULL character at the end
+    return atoi(inputBytesPtr);    // Call atoi function and return result
+  }
+  else
+    return -1;                           // Return -1 if there is no input
+}
+
+#ifdef testMem
+extern unsigned int __data_start;
+extern unsigned int __data_end;
+extern unsigned int __bss_start;
+extern unsigned int __bss_end;
+extern unsigned int __heap_start;
+extern void *__brkval;
+
+int freeMemory() {
+int free_memory;
+  
+    if((int)__brkval == 0)
+        free_memory = ((int)&free_memory) - ((int)&__bss_end);
+    else
+        free_memory = ((int)&free_memory) - ((int)__brkval);
+    return free_memory;
+};
+#endif
+
