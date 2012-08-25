d2447f228a7f240c4e717731ffff61155d7978bb
matthew venn <matt@mattvenn.net>
2012-08-25 14:55:04 +0100

    added argparse, timeouts

--- energyPlotter/leonardoTests/feed.py
+++ energyPlotter/leonardoTests/feed.py
@@ -3,32 +3,81 @@ import re
 import tty
 import time
 import string
+import argparse
+import signal
+TIMEOUT = 0.5 # number of seconds your want for timeout
 
-polar = True
+def handler():
+  print "timed out on serial read"
 
-gcode = open( "rosie.polar" )
-gcodes = gcode.readlines()
-serial = None
-port = '/dev/ttyACM0'
-serial = open( port, 'r+' )
-tty.setraw(serial);
-#print "wait for port"
-#//time.sleep( 1)
+signal.signal(signal.SIGALRM, handler)
 
-def readResponse():
+
+def readResponse(serial,timeout=10):
   response = ""
+  print "setting timeout to", timeout
   while string.find(response,"ok"):
-    response = serial.readline()
-    print response
-    if response == "ok\n":
-      print "got ok"
+    try:
+      if(timeout > 0):
+        signal.alarm(timeout)
 
-p = re.compile( "^#" )
+      response = serial.readline()
+      signal.alarm(0)
+      print response
+      if response == "ok\n":
+        print "got ok"
+    except:
+      print "timeout on read"
+      return
 
-for line in gcodes:
-  if p.match(line):
-    print "skipping line:", line
-  else:
-    print line
-    serial.write(line)
-    readResponse()
+def readFile(args):
+
+  try:
+    gcode = open( args.file)
+  except:
+    print "bad file"
+    exit(1)
+  gcodes = gcode.readlines()
+  serial = None
+  port = '/dev/ttyACM0'
+  serial = open( port, 'r+' )
+  tty.setraw(serial);
+
+  p = re.compile( "^#" )
+
+  if args.home:
+    serial.write("c")
+    readResponse(serial,0)
+
+   #speed and pwm
+  serial.write("p%d,%d" % (args.speed, args.pwm ))
+  readResponse(serial)
+
+  for line in gcodes:
+    if p.match(line):
+      print "skipping line:", line
+    else:
+      print line
+      serial.write(line)
+      readResponse(serial)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="feed polar files to polargraph robot")
+    parser.add_argument('--file',
+        action='store', dest='file', 
+        help="file to open")
+    parser.add_argument('--home',
+        action='store_const', const=True, dest='home', default=False,
+        help="home to start")
+    parser.add_argument('--pwm',
+        action='store', dest='pwm', type=int, default=80,
+        help="pwm to draw")
+    parser.add_argument('--speed',
+        action='store', dest='speed', type=int, default=3,
+        help="speed to draw")
+
+    args = parser.parse_args()
+
+    #set values, must be a better way of doing this
+    readFile(args)
--- energyPlotter/leonardoTests/preprocess.py
+++ energyPlotter/leonardoTests/preprocess.py
@@ -1,45 +1,98 @@
 #!/usr/bin/python
-
-gcode = open( "rosie.ngc" )
-xoffset = 1000
-ysub = 1000
-yoffset = 500
-scale = 30
+import argparse
 import re
 
-#start the file
-print "p3,100"
-print "c"
+def parse(args):
+  try:
+    gcode = open( args.file )
+  except:
+    print "bad file"
+    exit( 1 )
+  
 
-gcodes = gcode.readlines()
-startCode = re.compile( "^G([01])(?: X(\S+))?(?: Y(\S+))?(?: Z(\S+))?$")
-contCode =  re.compile( "^(?: X(\S+))?(?: Y(\S+))?(?: Z(\S+))?$")
-#p = re.compile( "G([01])(?= Z(\S+))")
-for line in gcodes:
-  s = startCode.match(line)
-  c = contCode.match(line)
-  gcode = 0
-  if s:
-    gcode = s.group(1)
-    x = s.group(2)
-    y = s.group(3)
-    z = float(s.group(4))
-    if z > 0 :
-      print "s600,2"
-    else:
-      print "s200,2"
-  elif c: 
-    try:
-      x = float(c.group(1))
-    except:
-      x = lastX 
-    try:
-      y = float(c.group(2))
-    except:
-      y = lastY
-#    z = float(c.group(3))
-#    print line
-    print "g%d,%d" %  (x*scale+xoffset,ysub - y*scale+yoffset) 
-    lastX = x
-    lastY = y
+  #start the file
+  #print "p3,100"
+  #print "c"
 
+  xmin = 100000
+  xmax = 0
+  ymin = 100000
+  ymax = 0
+
+  gcodes = gcode.readlines()
+  startCode = re.compile( "^G([01])(?: X(\S+))?(?: Y(\S+))?(?: Z(\S+))?$")
+  contCode =  re.compile( "^(?: X(\S+))?(?: Y(\S+))?(?: Z(\S+))?$")
+  #p = re.compile( "G([01])(?= Z(\S+))")
+  for line in gcodes:
+    s = startCode.match(line)
+    c = contCode.match(line)
+    gcode = 0
+    if s:
+      gcode = s.group(1)
+      x = s.group(2)
+      y = s.group(3)
+      z = float(s.group(4))
+      if z > 0 :
+        #don't draw
+        print "d0"
+      else:
+        #draw
+        print "d1"
+    elif c: 
+      try:
+        x = float(c.group(1))
+      except:
+        x = lastX 
+      try:
+        y = float(c.group(2))
+      except:
+        y = lastY
+  #    z = float(c.group(3))
+  #    print line
+      outx = x*args.scale+args.xoffset
+      outy = args.ysub - y*args.scale+args.yoffset
+      print "g%d,%d" %  (outx,outy) 
+      lastX = x
+      lastY = y
+      if outx < xmin:
+        xmin = outx 
+      if outx > xmax:
+        xmax = outx
+      if outy < ymin:
+        ymin = outy
+      if outy > ymax:
+        ymax = outy
+
+  print "# xmin %f xmax %f" % (xmin, xmax)
+  print "# ymin %f ymax %f" % (ymin, ymax)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="preprocesses ngc files for polargraph robot")
+    parser.add_argument('--file',
+        action='store', dest='file', 
+        help="file to open")
+    parser.add_argument('--scale',
+        action='store', dest='scale', type=float, default=6.3,
+        help="scale factor")
+    parser.add_argument('--ysub',
+        action='store', dest='ysub', type=int, default=1000,
+        help="need to reflect y axis atm, this is what the y is subtracted from")
+    parser.add_argument('--yoffset',
+        action='store', dest='yoffset', type=int, default=500,
+        help="how far to move the file on y axis")
+    parser.add_argument('--xoffset',
+        action='store', dest='xoffset', type=int, default=1000,
+        help="how far to move the file on x axis")
+    """
+    parser.add_argument('--stoneCutLength',
+        action='store', dest='stoneCutLength', type=int, default=280,
+        help="mm long side of material to cut the stones from")
+    parser.add_argument('--drawSizeLine',
+        action='store_const', const=True, dest='drawSizeLine', default=False,
+      """
+    args = parser.parse_args()
+
+    #set values, must be a better way of doing this
+    parse(args)
