7045d2752006694c13eca820edad547216d8027a
matthew venn <matt@mattvenn.net>
2012-08-27 18:52:24 +0100

    simple shell script to simulate how things will work in the futureeee

---
+++ energyPlotter/leonardoTests/sim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while true; do
+  if ./fetchlight.py; then
+    echo ---
+    rm -f squares.svg 
+    ./squares.py --load
+
+    if [ -f squares.svg ] ; then 
+     pycam squares.svg --export-gcode=squares.ngc --process-path-strategy=engrave -q
+     ./preprocess.py --file squares.ngc  --yoffset 1500 > squares.polar
+     ./feed.py --file squares.polar
+    fi
+  fi
+  sleep 5
+done
