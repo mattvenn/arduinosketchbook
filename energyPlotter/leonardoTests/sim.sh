c025af37f92f27173ba33ab5e0284abb8c8ad5cb
matthew venn <matt@mattvenn.net>
2012-08-27 19:00:34 +0100

    added speed control and extra line division

--- energyPlotter/leonardoTests/sim.sh
+++ energyPlotter/leonardoTests/sim.sh
@@ -8,8 +8,9 @@ while true; do
     if [ -f squares.svg ] ; then 
      pycam squares.svg --export-gcode=squares.ngc --process-path-strategy=engrave -q
      ./preprocess.py --file squares.ngc  --yoffset 1500 > squares.polar
-     ./feed.py --file squares.polar
+     ./feed.py --file squares.polar --speed 6
     fi
   fi
   sleep 5
+    echo ---
 done
