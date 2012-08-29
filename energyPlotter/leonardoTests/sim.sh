d55bdaad51dae3d359ed3943adacdd2405b1cb27
matthew venn <matt@mattvenn.net>
2012-08-29 15:44:57 +0100

    now concats the svg files

--- energyPlotter/leonardoTests/sim.sh
+++ energyPlotter/leonardoTests/sim.sh
@@ -2,13 +2,15 @@
 while true; do
   if ./fetchlight.py; then
     echo ---
-    rm -f squares.svg 
     ./squares.py --load
 
     if [ -f squares.svg ] ; then 
      pycam squares.svg --export-gcode=squares.ngc --process-path-strategy=engrave -q
      ./preprocess.py --file squares.ngc  --yoffset 1500 > squares.polar
      ./feed.py --file squares.polar --speed 6
+      timestamp=$(date +%s)
+      mv squares.svg history/$timestamp.svg
+      ./concat.py
     fi
   fi
   sleep 5
