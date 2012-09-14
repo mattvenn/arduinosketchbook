47b5e051ad7bb501a6255afae5dcfe2aa4daec35
matthew venn <matt@mattvenn.net>
2012-09-14 18:46:21 +0100

    started

---
+++ h2mdk_test/h2mdk_test.ino
@@ -0,0 +1,23 @@
+/* h2mdk defs */
+
+ static const int PURGE = 3;
+    static const int LOAD = 4;  // 3w can disconnect load
+    static const int SHORT = 5;
+    static const int STATUS_LED = 6;
+  #endif
+//analog pins
+    static const int VOLTAGE_SENSE = A1;
+    static const int CURRENT_SENSE = A2;
+    static const int CAP_V_SENSE = A3;
+
+/* extra defs */
+
+    static const int supplyCurrent= A0;
+    static const int outputVoltage = A4;
+    
+    static const int connectSupply = 7;
+    static const int connectLoad1 = 8;
+    
+    
+    
+    
