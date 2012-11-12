c9b6eb9a026818cdcb70d7cc26ba2e25b126947a
matthew venn <matt@mattvenn.net>
2012-11-12 16:27:57 +0000

    custom chars for on and offline

--- wirelessAudio/mibo_master/mibo_master.ino
+++ wirelessAudio/mibo_master/mibo_master.ino
@@ -5,7 +5,7 @@
 #include <SoftwareSerial.h>
 LiquidCrystal lcd(12, 11, 5, 4, 7, 6);
 
-const int switchDebounceTime = 500;
+const int switchDebounceTime = 200;
 
 const int numNodes = 10;
 const int playButton = 8;
@@ -14,76 +14,103 @@ const int volumeKnob = A0;
 double lastKnobTime, lastPollTime;
 double nodeStatus [numNodes];
 int volume, lastVolume;
+boolean isplaying = false;
 
 SoftwareSerial xbeeSerial(XBEE_RX,XBEE_TX);
+
+ int onlineChar = 0;
+ int offlineChar = 1;
+
+byte offline[8] = {
+  B11111,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
+  B11111,
+};
+
+byte online[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
 
 void setup()
 {
   Serial.begin(9600);
   xbeeSerial.begin(9600);
   Serial.println( "started xbee master" );
+
   lcd.begin(16, 2);
-  displayStartScreen();
+  lcd.createChar(onlineChar, online);  
+  lcd.createChar(offlineChar, offline);  
   
+  displayStartScreen();
+
   pinMode( stopButton, INPUT );
   pinMode( playButton, INPUT );
-  
+
+  stopplay();
   pollNodes();
-  stopNodes();
 }
 void loop()
 {
-  if( Serial.available() )
-  {
-    delay(50);
-    char command = Serial.read();
-    switch( command )
-    {
-      case 'i':
-        pollNodes();
-        break;
-      case 's':
-        startNodes();
-        break;
-      case 'e' :
-        stopNodes();
-        break;
-      default:
-        Serial.println( "bad command:" );
-        Serial.println( command );
-    }
-  }
   if( digitalRead( playButton ) )
   {
-    startNodes();
+    play();
+    
   }
   else if( digitalRead( stopButton ) )
   {
-    stopNodes();
+    stopplay();
   }
 
-  
-  
+
+  //volume knob stuff, only check every 200ms
   if( millis() - lastKnobTime > 200 )
   {
     volume = map( analogRead( volumeKnob ), 0 , 1024, 100, 0 );
-  
     lastKnobTime = millis();
     if( lastVolume != volume ) // knob has changed
     {
+      //only send if knob changed
       sendVolume();
     }
     lastVolume = volume;
-
   }
-  
+
+  //send message to all nodes
   if( millis() - lastPollTime > 1000 )
   {
     lastPollTime = millis();
     pollNodes();
   }    
- }
+}
 
+void stopplay()
+{
+
+    isplaying = false;
+    Serial.println( "stopping" );
+    lcd.setCursor(0,1);
+    lcd.print( " stopped");
+    delay(switchDebounceTime);
+}
+void play()
+{
+    isplaying = true;
+    Serial.println( "starting" );
+    lcd.setCursor(0,1);
+    lcd.print( " playing");
+    delay(switchDebounceTime);
+}
 void sendVolume()
 {
   xbeeSerial.print( "cv" );
@@ -91,39 +118,20 @@ void sendVolume()
   Serial.print( "sending vol:" );
   Serial.println( volume );
   lcd.setCursor(8,1);
-  lcd.print( "vol:    ");
-  lcd.setCursor(13,1);
+  lcd.print( "        ");
+  lcd.setCursor(12,1);
   lcd.print( volume );
-  
-  delay(200);
-}
-void startNodes()
-{
-  xbeeSerial.print( "cs" );
-  delay(200);
-  xbeeSerial.print( "cs" );
-  Serial.println( "starting" );
-  lcd.setCursor(0,1);
-  lcd.print( "playing");
-  delay(switchDebounceTime);
-}
-void stopNodes()
-{
-  xbeeSerial.print( "ce" );
-  delay(200);
-  xbeeSerial.print( "ce" );
-  Serial.println( "ending" );
-  lcd.setCursor(0,1);
-  lcd.print( "stopped");
-  delay(switchDebounceTime);
 }
 
 
 void pollNodes()
 {
-  Serial.println( "checking for online nodes..." );
-  xbeeSerial.print( "ci" );
-  
+  //deal with packet loss by continualling telling nodes what to do!
+  if( isplaying )
+    xbeeSerial.print( "cs" );
+  else
+    xbeeSerial.print( "ce" );
+
   while( xbeeSerial.available() )
   {
     if( xbeeSerial.read() == 'm' )
@@ -134,13 +142,13 @@ void pollNodes()
       nodeStatus[node] = millis();
     }
   }
-  lcd.setCursor(0,0);
+  lcd.setCursor(3,0);
   for( int node = 0; node < numNodes ; node ++ )
   {
-    if( millis() - nodeStatus[node] > 5000 )
-      lcd.print(".");
+    if( millis() - nodeStatus[node] < 5000 )
+      lcd.write( onlineChar );
     else
-      lcd.print("*");
+      lcd.write( offlineChar );
   }
 }
 
@@ -148,7 +156,10 @@ void displayStartScreen()
 {
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print( "mibo master");
+  lcd.print( "  mibo  master");
+  lcd.setCursor(0,1);
+  lcd.print( "    starting");
   delay(1000);
   lcd.clear();
 }
+
