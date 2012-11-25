046f2c35e3103fd2dd4b52891f7b67d6d0227306
matthew venn <matt@mattvenn.net>
2012-11-25 18:07:40 +0000

    changed the protocol and hence code

--- energyPlotter/leonardoTests/testServo.ino
+++ energyPlotter/leonardoTests/testServo.ino
@@ -1,31 +1,34 @@
-
-void pulsePower( int pulseLength, int delayTime )
+void initServo()
 {
-  //Serial.print( "pulsing servo line: " );
-  //Serial.println( delayTime );
-
-  //turn on power to the board
   setPowerPin(HIGH);
+}
+void penDown()
+{
+  pulsePower(2);
+delay(50);
+  pulsePower(2);
+  delay(1000); //these could be made shorter if the gondola code is better
+}
 
+void penUp()
+{
+  pulsePower(2);
+  delay(100);
+  pulsePower(2);
+    delay(1000); //these could be made shorter
+}
+
+void pulsePower(  int delayTime )
+{
+  setPowerPin(LOW);
   //wait the specified length
   delay(delayTime);
+  setPowerPin(HIGH);
 
-  //send the signal pulse
-  setPowerPin( LOW   );
-  delay( pulseLength );
-  setPowerPin( HIGH );
-
-  //wait for servo to move, think this is about right
-  delay(2*delayTime);
-
-  //then turn off the power
-  setPowerPin(LOW);
-  //wait for a bit longer to allow board to reset (multiple calls can stop the servo working properly)
-  delay(500);
 }
 void setPowerPin( boolean state )
 {
-  digitalWrite(SERVO, !state );
+  digitalWrite(SERVO, state );
 //  digitalWrite(led, ! state );
 }
 
