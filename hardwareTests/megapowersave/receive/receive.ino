c698b25e9621e3bf79e335e2adc71190dd39288a
Matt Venn <matt@mattvenn.net>
2013-03-14 12:31:46 +0000

    was working, now isnt

--- hardwareTests/megapowersave/receive/receive.ino
+++ hardwareTests/megapowersave/receive/receive.ino
@@ -1,24 +1,134 @@
+//this is the low power arduino
+/*
+ + todo: brown out disable
+ 
+  turning off peripherals doesn't seemt o make a difference to wake up time.
+ 
+ 9600 baud is 9600 bits per second or 10khz ish
+ digitalread/write is 1mhz ish?
+
+  sleep mode: pwr_down
+    at 9600 baud, miss first 5 chars of the wakeup byte
+    
+  are external interrupts faster than pinchange?
+
+  todo:
+   check external ints are they faster
+  look at different sleep modes
+  find out if we can miss any part of the first byte? 
+*/
+#include <PinChangeInt.h> //see details on this to reduce memory footprint
+#include <avr/sleep.h>
+#include <avr/power.h>
+
+#define PINCHANGE
+const int capture = 100;
+boolean buff [capture];
+byte r;
+boolean ready = false;
+
+#define led 13
+boolean led_state;
+long timer;
 void setup()
 {
-  //Serial1.begin(9600, SERIAL_8N2);
-  Serial1.begin(9600);
-    Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(13,OUTPUT);
-  pinMode(0,INPUT);
+  Serial.println("starting");
+    
 }
+
+void attachInt()
+{
+  #ifdef PINCHANGE
+    PCintPort::attachInterrupt(0, &wakeUpNow, FALLING);  // add more attachInterrupt code as required
+  #else
+  attachInterrupt(0,wakeUpNow, FALLING); // use interrupt 0 (pin 2) and run function
+  #endif
+}
+
+void detachInt()
+{
+  #ifdef PINCHANGE
+    PCintPort::detachInterrupt(0);
+  #else
+    detachInterrupt(0); 
+  #endif
+
+}
+
+void power_down()
+{
+  Serial.println("power down");
+  delay(50); //allow message to go out
+  //power_all_disable();
+  
+  power_adc_disable();
+   power_spi_disable();
+   power_timer0_disable();
+   power_timer1_disable();
+   power_usart0_disable();
+   power_timer2_disable();
+   power_twi_disable();
+   
+}
+
+
+void power_up()
+{
+  power_all_enable();
+ // Serial.begin(9600);
+  Serial.println("powered up");
+}
+
 void loop()
 {
- 
-    if(Serial1.available())
-    {Serial.print("got:");
-    while(Serial1.available())
+    if(millis()-timer>500)
     {
-      Serial.print((char)Serial1.read());
-    }
-    digitalWrite(13,HIGH);
-    delay(100);
-    digitalWrite(13,LOW);
-    }
- 
+       timer = millis();
+       digitalWrite(led,led_state);
+       led_state = !led_state;
 
- }
+    }
+    /*
+    if( ready )
+    {
+      print_buff();
+      ready = false;
+    }
+    */
+    if( Serial.available() )
+    {
+      char c = Serial.read();
+      switch( c)
+      {
+        case 's':
+          Serial.println("sleep");
+          delay(50);
+         // power_down();
+
+          attachInt();
+          sei();
+          sleep_now();
+          
+          Serial.println("woke");
+          //detachInt();
+        //  power_up();
+          print_buff();
+          break;
+        default:
+          Serial.print("unknown command:");Serial.println(c);
+          break;
+      }
+    }
+}
+
+void print_buff()
+{
+  for( int i = 0; i < capture; i ++ )
+  {
+    Serial.print(buff[i]);
+    buff[i]=false;
+  }
+  Serial.println("");
+}
