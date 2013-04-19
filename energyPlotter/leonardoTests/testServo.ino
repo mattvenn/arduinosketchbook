31dfbb145430fd1272f9be0d8125b8ce81ec52e6
matthew venn <matt@mattvenn.net>
2013-04-19 18:16:55 +0100

    reduced servo time, and fixed pen up at reset

--- energyPlotter/leonardoTests/testServo.ino
+++ energyPlotter/leonardoTests/testServo.ino
@@ -1,13 +1,15 @@
 void initServo()
 {
-  penDown();
+  setPowerPin(HIGH);
+  delay(1000);
+  penUp();
 }
 void penUp()
 {
   pulsePower(2);
 delay(100);
   pulsePower(2);
-  delay(1000); //these could be made shorter if the gondola code is better
+  delay(500); //these could be made shorter if the gondola code is better
 }
 
 void penDown()
@@ -15,7 +17,7 @@ void penDown()
   pulsePower(2);
   delay(50);
   pulsePower(2);
-    delay(1000); //these could be made shorter
+    delay(500); //these could be made shorter
 }
 
 void pulsePower(  int delayTime )
