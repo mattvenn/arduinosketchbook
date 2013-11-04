19f6bc2a44af63072f4335f50dec91ccbcb6f3d9
Matt Venn <matt@mattvenn.net>
2013-11-04 15:07:00 +0000

    turned off current after latch

--- arcola_remote_monitor/relays.ino
+++ arcola_remote_monitor/relays.ino
@@ -3,6 +3,9 @@ void relay_setup()
 {
   SPI.begin();
   SPI.setClockDivider(SPI_CLOCK_DIV16);
+  for(int i = 0; i<4; i ++)
+    relay_control(i,false);
+
 }
 void test_relays()
 {
@@ -19,16 +22,39 @@ void test_relays()
 }
 
 
+
 //doesn't preserve other relay's state
 //sometimes doesn't work - fixed now that spi clock lowered?
 void relay_control(int relay, boolean state)
 {
   digitalWrite(relay_cs,LOW);
   if(state)
+  {
+    //latch it
     SPI.transfer(0b10 << relay * 2);
+    digitalWrite(relay_cs,HIGH);
+
+    delay(1);
+
+    //remove drive current
+    digitalWrite(relay_cs,LOW);
+    SPI.transfer(0b00 << relay * 2);
+  }
   else
+  {
+    //latch
     SPI.transfer(0b01 << relay * 2);
+    digitalWrite(relay_cs,HIGH);
+
+    delay(1);
+
+    //remove drive current
+    digitalWrite(relay_cs,LOW);
+    SPI.transfer(0b00 << relay * 2);
+  }
   digitalWrite(relay_cs,HIGH);
 }
+
+
 
 
