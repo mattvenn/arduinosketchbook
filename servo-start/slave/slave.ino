5c712a1e1c4af0dd3d82262ff56e81e54beeae6e
matt venn <matt@mattvenn.net>
2015-12-01 17:59:46 +0000

    timer setup in h file

--- servo-start/slave/slave.ino
+++ servo-start/slave/slave.ino
@@ -10,6 +10,7 @@ enum SlaveCommand {SLV_LOAD, SLV_SET_POS};
 #define RS485Transmit    HIGH
 #define RS485Receive     LOW
 
+#include "timer.h"
 #include "buttons.h"
 #include "utils.h"
 #include "pindefs.h"
@@ -20,11 +21,7 @@ SoftwareSerial master_serial(SS_RX, SS_TX); // RX, TX
 
 const float mm_to_pulse = 35.3688;
 
-volatile bool calc;
-volatile byte timer2_overflows = 0;
-uint8_t timer2_overflow = 0;
-uint8_t timer2_remainder = 0;
-int timer1_counter = 0;
+volatile bool calc = false;
 
 //pid globals
 int pwm = 128;
@@ -42,27 +39,10 @@ float kp = .45;
 float ki = 0.000;
 float kd = .25;
 
-//interrupt service routine 
-//interrupt service routine 
-ISR(TIMER2_OVF_vect)        
-{
-    timer2_overflows ++;
-    if(timer2_overflows == timer2_overflow)
-    {
-        //preload timer
-        TCNT2 = timer2_remainder;
-    }
-    if(timer2_overflows > timer2_overflow)
-    {
-        timer2_overflows = 0;
-        calc = true;
-        TCNT2 = 0;
-    }
-}
-
 void setup()
 {
     buttons_setup();
+    setup_timer2();
     Serial.begin(115200);
     master_serial.begin(57600); // 115200 too fast for reliable soft serial
     pinMode(LED, OUTPUT);
@@ -75,24 +55,8 @@ void setup()
     pinMode(REV, OUTPUT);
     digitalWrite(REV,LOW);
 
-    // timer 2 setup
-    TCCR2A = 0;
-    TCCR1B = 0;
 
-    // set timer prescalar to 1024
-    TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
-    // turn on interrupt overflow
-    TIMSK2 |= (1 << TOIE2);
-
-    // preload timer2 (8 bits)
-    timer2_overflow = 1;
-    timer2_remainder = 256 - 56;
-    TCNT2 = 0;
-
-    // set pwm frequency on pins 9&10 (timer1) to 31250Hz
-    TCCR1B = TCCR1B & 0b11111000 | 0x01;
-
-
+    // pid init
     b0 = kp+ki+kd;
     b1 = -kp-2*kd;
     b2 = kd;
@@ -177,13 +141,13 @@ void loop()
         switch(data.command)
         {
             case SLV_LOAD:
-                Serial.print("loaded:");
-                Serial.println(data.pos);
+                //Serial.print("loaded:");
+                //Serial.println(data.pos);
                 posref = data.pos * mm_to_pulse;
                 break;
             case SLV_SET_POS:
-                Serial.print("setpos:");
-                Serial.println(data.pos);
+                //Serial.print("setpos:");
+                //Serial.println(data.pos);
                 posref = data.pos * mm_to_pulse;
                 myEnc.write(posref);
                 break;
